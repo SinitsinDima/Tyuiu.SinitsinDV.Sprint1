1086dbc5651d66e40b9ab9fe7e7093725234ab48
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-09-29 22:31:16 +0500

    Task4

--- Tyuiu.SinitsinDV.Sprint1.Task4.V11.Lib/DataService.cs
+++ Tyuiu.SinitsinDV.Sprint1.Task4.V11.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.SinitsinDV.Sprint1.Task4.V11.Lib
     {
         public double Calculate(double x, double y)
         {
-            var res = Math.Atan(x) / Math.Pow(5, y);
+            var res = Math.Atan(x) / Math.Exp(y);
             var res2 = Math.Round(res, 3);
             return res2;
 
--- Tyuiu.SinitsinDV.Sprint1.Task4.V11.Test/DataServiceTest.cs
+++ Tyuiu.SinitsinDV.Sprint1.Task4.V11.Test/DataServiceTest.cs
@@ -9,8 +9,9 @@ namespace Tyuiu.SinitsinDV.Sprint1.Task4.V11.Test
         {
             DataService ds = new DataService();
             double x = 5;
-            double y = 3;
-            double wait = 0.011;
+            double y = 5;
+            var exp = Math.Exp(y);
+            var wait = 0.009;
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
             
